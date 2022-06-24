c34c0ebfc41ca163669a0f975a9228c8e0e648e8
KyawZInThant22 <kyawthantver2@gmail.com>
2022-06-24 23:10:51 +0630

    fix responsive

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
+
 const ContactForm = () => {
   return (
     <div className="lg:w-[40rem]">
       <h1 className="ContactHeader">Contact Form</h1>
       <form className="space-y-6 lg:mt-12 mt-6">
-          <input className=" contact-class  "type="text" placeholder="Name" />
-          <input className="block contact-class" type="text" placeholder="Phone" />
-          <textarea placeholder="Message" className="contact-class w-full  lg:w-[80%] resize-none h-[10rem]"></textarea>
+          <input className="   block p-3 rounded lg:w-[80%] w-full border border-solid border-nonWhite text-xl "type="text" placeholder="Name" />
+          <input className=" block p-3 rounded lg:w-[80%] w-full border border-solid border-nonWhite text-xl  " type="text" placeholder="Phone" />
+          <textarea placeholder="Message" className=" block p-3 rounded  border border-solid border-nonWhite text-xl   w-full  lg:w-[80%] resize-none h-[10rem]"></textarea>
           <button className="bg-primary py-3 uppercase px-6   text-xl rounded text-white ">Send Message</button>
       </form>
     </div>
